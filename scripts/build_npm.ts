85132c9abd9bc126a52a42f390ea84160ded051e
Matt Kane <m@mk.gg>
2023-02-28 20:51:23 +0000

    feat: initial npm support

---
+++ scripts/build_npm.ts
@@ -0,0 +1,39 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.22.0/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./src/index.ts"],
+  outDir: "./npm",
+  shims: {
+    deno: {
+      test: "dev",
+    },
+  },
+  rootTestDir: "./test-node",
+  package: {
+    // package.json properties
+    name: "@unpic/pixels",
+    version: Deno.args[0]?.replace(/^v/, ""),
+    description: "Get raw pixels from an image",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/ascorbic/get-pixels.git",
+    },
+    bugs: {
+      url: "https://github.com/ascorbic/get-pixels/issues",
+    },
+    devDependencies: {
+      "@types/node": "latest",
+      "@types/pngjs": "latest",
+    },
+    publishConfig: {
+      access: "public",
+    },
+  },
+});
+
+// post build steps
+// Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README-npm.md", "npm/README.md");
---
+++ src/index.test.ts
@@ -0,0 +1,42 @@
+import { assertEquals } from "https://deno.land/std@0.172.0/testing/asserts.ts";
+import { getDataFromUrl, getFormat, getPixels } from "../mod.ts";
+import { encode } from "https://esm.sh/blurhash@2.0.4";
+
+Deno.test("Size detection", async () => {
+  const jpgData = await getPixels(
+    "https://res.cloudinary.com/demo/image/upload/c_lfill,w_200,h_100/dog.jpg",
+  );
+  assertEquals(jpgData.width, 200);
+  assertEquals(jpgData.height, 100);
+
+  const pngData = await getPixels(
+    "https://res.cloudinary.com/demo/image/upload/c_lfill,w_200,h_100/dog.png",
+  );
+  assertEquals(pngData.width, 200);
+  assertEquals(pngData.height, 100);
+});
+
+Deno.test("Format detection", async () => {
+  const jpgFormat = getFormat(
+    await getDataFromUrl(
+      "https://res.cloudinary.com/demo/image/upload/c_lfill,w_200,h_100/dog.jpg",
+    ),
+  );
+  assertEquals(jpgFormat, "jpg");
+
+  const pngFormat = getFormat(
+    await getDataFromUrl(
+      "https://res.cloudinary.com/demo/image/upload/c_lfill,w_200,h_100/dog.png",
+    ),
+  );
+  assertEquals(pngFormat, "png");
+});
+
+Deno.test("Blurhash", async () => {
+  const jpgData = await getPixels(
+    "https://res.cloudinary.com/demo/image/upload/c_lfill,w_200,h_100/dog.jpg",
+  );
+  const data = Uint8ClampedArray.from(jpgData.data);
+  const blurhash = encode(data, jpgData.width, jpgData.height, 4, 4);
+  assertEquals(blurhash, "UAFih,Im00-?QD$+NKE100xv~EM^cENEs+?H");
+});
